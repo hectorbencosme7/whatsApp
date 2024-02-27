4c3df159f52d3bbef1e9755df4a3e8ef100daad3
hectorbencosme7 <hectorxd7@gmail.com>
2024-02-27 00:06:18 -0400

    Added Camara and Add Media icons to ChatScreen

--- Screens/ChatScreen.js
+++ Screens/ChatScreen.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground} from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Button, TextInput, TouchableOpacity} from "react-native";
 import backgroundImage from '../assets/Images/droplet.jpeg'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Feather } from "@expo/vector-icons";
+import colors from "../Constants/colors.js"
+
 
 const ChatScreen = props =>{
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}
+        edges={['bottom','left','right']}>
               <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
 
-             </ImageBackground>      
-        </View>
+             </ImageBackground>  
+
+           
+            <View style={styles.inputContainer}>
+               <TouchableOpacity onPress={()=>{console.log("I was pressed")}}>
+                 <Feather name="plus" size={24} color={colors.blue} />
+               </TouchableOpacity>
+
+                <TextInput/>
+
+                <TouchableOpacity onPress={()=>{console.log("I was pressed")}}>
+                 <Feather name="camera" size={24} color={colors.blue} />
+               </TouchableOpacity>
+            </View>
+            
+
+        </SafeAreaView>
     )
 };
 
@@ -19,6 +39,12 @@ const styles = StyleSheet.create({
     },
     backgroundImage:{
         flex:1
+    },
+    inputContainer:{
+        flexDirection: 'row',
+        paddingVertical: 8,
+        paddingHorizontal: 10,
+        height: 50
     }
 });
 export default ChatScreen;
